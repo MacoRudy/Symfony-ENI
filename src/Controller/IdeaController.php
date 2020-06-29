05afb26d430e03cf9274c989b666c3b4475dbfa9
Macorigh <ralpina@hotmail.com>
2020-06-29 12:28:44 +0200

    Fin TP6

--- src/Controller/IdeaController.php
+++ src/Controller/IdeaController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Idea;
+use App\Form\IdeaType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IdeaController extends Controller
@@ -12,16 +16,50 @@ class IdeaController extends Controller
      */
     public function list()
     {
-        return $this->render("default/list.html.twig");
+        $ideaRepo = $this->getDoctrine()->getRepository(Idea::class);
+        $ideas = $ideaRepo->findBy(["isPublished" => true], ["dateCreated" => "DESC"]);
+
+
+        return $this->render("default/list.html.twig", [
+            "ideas" => $ideas
+        ]);
     }
 
     /**
      * @Route("/detail/{id}", name="idea_detail", requirements={"id":"\d+"})
      */
-    public function detail($id)
+    public function detail($id, Request $request)
     {
+        $ideasRepo = $this->getDoctrine()->getRepository(Idea::class);
+        $idea = $ideasRepo->find($id);
+        return $this->render("default/detail.html.twig", ["idea" => $idea]);
 
-        return $this->render("default/detail.html.twig");
+    }
+
+    /**
+     * @Route("/add", name="add_idea")
+     */
+    public function add(EntityManagerInterface $em, Request $request)
+    {
+        $idea = new Idea();
+        $ideaForm = $this->createForm(IdeaType::class, $idea);
+        $idea->setDateCreated(new \DateTime());
+        $idea->setIsPublished(true);
+
+        $ideaForm->handleRequest($request);
+
+        if ($ideaForm->isSubmitted() && $ideaForm->isValid()) {
+            $em->persist($idea);
+            $em->flush();
+            $this->addFlash("success", "Idea successfully added!");
+            return $this->redirectToRoute("idea_detail", [
+                    "id" => $idea->getId()
+                ]
+            );
+        }
+
+
+        return $this->render("default/add.html.twig", ["ideaForm" => $ideaForm->createView()]);
 
     }
 
